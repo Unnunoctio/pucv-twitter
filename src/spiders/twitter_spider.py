0af22f22b79732fdd428d615a23a9e9462a414d3
Unnunoctio <unnunoctio.dev@gmail.com>
2024-12-15 03:30:16 -0300

    feat: :sparkles: start twitter login

--- src/spiders/twitter_spider.py
+++ src/spiders/twitter_spider.py
@@ -1,3 +1,5 @@
+from playwright.sync_api import Browser, sync_playwright
+
 from classes.post import Post
 from classes.search import Search
 from classes.user import User
@@ -9,8 +11,32 @@ class TwitterSpider:
     def __init__(self, user: User):
         self.user = user
 
-    def login(self):
-        pass
+    def login(self, browser: Browser):
+        try:
+            page = browser.new_page()
+            page.goto(self.LOGIN_URL)
+
+            page.wait_for_selector('input[name="text"]')
+            page.fill('input[name="text"]', self.user.email)
+            page.click('text=Next')
+            page.wait_for_timeout(1000)
+
+            if page.is_visible('input[name="password"]'):
+                page.fill('input[name="password"]', self.user.password)
+                page.click('text=Log in')
+            else:
+                page.fill('input[name="text"]', self.user.username)
+                page.click('text=Next')
+
+                page.wait_for_selector('input[name="password"]')
+                page.fill('input[name="password"]', self.user.password)
+                page.click('text=Log in')
+
+            page.wait_for_timeout(3000)
+            return
+        except Exception as e:
+            browser.close()
+            raise Exception(e)
 
     def generate_search_url(self, keyword: str | None, account: str | None) -> str:
         if (keyword is not None) and (account is not None):
@@ -21,4 +47,14 @@ class TwitterSpider:
             return f"https://x.com/search?q=from%3A{account}&src=typed_query&f=live"
 
     def get_posts(self, search: Search) -> list[Post]:
-        pass
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            self.login(browser)
+
+            page = browser.new_page()
+            page.goto(self.generate_search_url(search.keyword, search.account))
+            page.wait_for_timeout(2000)
+
+            browser.close()
+
+            return []
