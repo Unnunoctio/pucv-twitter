30d9da38e158ee369bf94cf1ebb340034a670b15
Unnunoctio <unnunoctio.dev@gmail.com>
2024-12-16 18:32:57 -0300

    feat: :sparkles: end index

--- src/index.py
+++ src/index.py
@@ -3,6 +3,8 @@ import time
 from classes.search import Search
 from classes.user import User
 from config import (
+    EMAIL_ACCOUNT,
+    EMAIL_SEND,
     SEARCH_ACCOUNT,
     SEARCH_END,
     SEARCH_KEYWORD,
@@ -12,25 +14,31 @@ from config import (
     TWITTER_USERNAME,
 )
 from spiders.twitter_spider import TwitterSpider
+from utils.email import send_email
+from utils.excel import write_excel
 
 try:
     start_time = time.time()
 
+    # TODO: VALIDAR DATOS
     user = User(username=TWITTER_USERNAME, email=TWITTER_EMAIL, password=TWITTER_PASSWORD)
-    print(user)
-
     search = Search(keyword=SEARCH_KEYWORD, account=SEARCH_ACCOUNT, start_date=SEARCH_START, end_date=SEARCH_END)
-    print(search)
 
+    # TODO: OBTENER POSTS
     twitter_spider = TwitterSpider(user)
     posts = twitter_spider.get_posts(search)
-    print(len(posts))
 
-    # SEND EMAIL WITH POSTS
+    # TODO: CREAR EXCEL
+    file_name = f"twitter_posts_{search.start_date.date()}_{search.end_date.date()}.xlsx"
+    write_excel(file_name, posts)
+
+    # TODO: ENVIAR EMAIL
+    email_destinatary = EMAIL_SEND if EMAIL_SEND is not None else EMAIL_ACCOUNT
+    send_email(email_destinatary, search, file_name)
 except ValueError as e:
     print(e)
     exit(1)
 finally:
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Time de ejecución: {elapsed_time:.4f} segundos")
+    print(f"Tiempo de ejecución: {elapsed_time:.4f} segundos")
